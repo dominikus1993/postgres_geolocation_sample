7d207a79bb012d8e04280549c3b0ae37a4d1d397
Dominik.Kotecki <dominikus1910@gmail.com>
2023-06-04 18:20:32 +0200

    .

--- src/Geolocation.Infrastructure/ServiceCollectionExtensions.cs
+++ src/Geolocation.Infrastructure/ServiceCollectionExtensions.cs
@@ -1,5 +1,7 @@
 using Geolocation.Core.Repositories;
+using Geolocation.Infrastructure.EntityFramework;
 using Geolocation.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -10,6 +12,16 @@ public static class ServiceCollectionExtensions
     public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
         services.AddScoped<IShopsFilter, EfShopsFilter>();
+        services.AddPooledDbContextFactory<ShopsDbContext>(builder =>
+        {
+            builder.UseNpgsql(configuration.GetConnectionString("ShopsDb"), optionsBuilder =>
+            {
+                optionsBuilder.EnableRetryOnFailure(5);
+                optionsBuilder.CommandTimeout(720);
+                optionsBuilder.UseNetTopologySuite();
+                optionsBuilder.MigrationsAssembly(typeof(ShopsDbContext).Assembly.FullName);
+            });
+        });
         return services;
     }
 }
